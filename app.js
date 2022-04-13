277d4b29e7844f9644b6692e8a83c01708072222
Guido <guido.maimone7@gmail.com>
2022-04-13 19:36:15 -0300

    subiendo proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(3001, () => console.log("Servidor corriendo en el puerto http://localhost:3001"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
